ca3b92aa4be0c41f84576886a9d673b5a352284c
Chokchai <93382971+ArmLegend@users.noreply.github.com>
2022-11-29 07:54:16 +0000

    format code to pass lint

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import pic from "./Item/placeholder.png"
+import pic from "./Item/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
