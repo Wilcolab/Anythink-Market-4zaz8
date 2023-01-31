39c24edfe20d932664faa71ba6655d4bfad4ba07
Chokchai <93382971+ArmLegend@users.noreply.github.com>
2023-01-31 04:30:53 +0000

    apply theme provider to an app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,14 +7,19 @@ import { store, history } from "./store";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 
+import {ThemeProvider} from "styled-components"
+import theme from "./styles/theme"
+
 import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </ThemeProvider>
     </ConnectedRouter>
   </Provider>,
 
