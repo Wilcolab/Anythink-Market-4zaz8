48006c000dd027ef9552c4e0db0cedee8a719bf6
WilcoApp <hello@wilco.gg>
2023-01-31 04:11:12 +0000

    Adds GlobalStyle file

---
+++ frontend/src/styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  background-image: none;
+  background-color: red;
+  background-position: top;
+  background-repeat: no-repeat;
+  }
+`;
+
+export default GlobalStyle;
