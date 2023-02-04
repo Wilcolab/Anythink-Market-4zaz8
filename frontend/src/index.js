301278f4a605e98c93d72442f6de52285c86dd3c
WilcoApp <hello@wilco.gg>
2023-02-04 10:35:25 +0000

    Robocrop

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,19 +7,14 @@ import { store, history } from "./store";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 
-import {ThemeProvider} from "styled-components"
-import theme from "./styles/theme"
-
 import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-          <Switch>
-            <Route path="/" component={App} />
-          </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
     </ConnectedRouter>
   </Provider>,
 
